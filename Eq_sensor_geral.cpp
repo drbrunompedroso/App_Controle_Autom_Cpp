aaadda7c8db558a263506f60da20f31b1caba33a
drbrunompedroso <drbrunompedroso@gmail.com>
2022-09-15 19:57:31 -0300

    Atualização Sensor

--- Eq_sensor_geral.cpp
+++ Eq_sensor_geral.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <conio.h>
+
 float y_max, 
 	  y_min, x_max, 
 	  x_min, coef_ang,
@@ -20,60 +20,56 @@ void calc_sensor()
 
 main()
 {
-	while(1)
-	{
-		printf("\n""transdutor - TENSAO X CORRENTE""\n");
-		printf("\n""para corrente (A) - para tensao (V)""\n");
-		scanf("%s", &signal);
+	
+printf("\n""transdutor - TENSAO X CORRENTE""\n");
+printf("\n""para corrente (A) - para tensao (V)""\n");
+scanf("%s", &signal);
 
-			switch(signal)
-			{
-				case 'V':
-					printf("\n""transdutor - TENSAO""\n");
-      				printf("ENTRE COM O VALOR MAXIMO DE TENSAO:""\n");
-      				scanf("%f", &y_max);
-      				printf("ENTRE COM VALOR MINIMO DE TENSAO:""\n");
-      				scanf("%f", &y_min);
-      				printf("ENTRE COM O VALOR MAXIMO DA VARIAVEL MEDIDA:""\n");
-      				scanf("%f", &x_max);
-      				printf("ENTRE COM VALOR MINIMO DA VARIAVEL MEDIDA:""\n");
-      				scanf("%f", &x_min);
-      				printf("ENTRE COM O VALOR DO SENSOR:""\n");
-      				scanf("%f", &sensor_signal);
-      
-      				calc_cang();
-      				calc_sensor();  
-                 
-   	  				printf("\n""CONVERSAO: %.2f",sensor_final);
-   	  				getch();
-   	  			break;
-   	  			
-   	  			case 'A':
-   	  				printf("\n""transdutor - CORRENTE""\n");
-      				printf("ENTRE COM O VALOR MAXIMO DE CORRENTE:""\n");
-      				scanf("%f", &y_max);
-      				printf("ENTRE COM VALOR MINIMO DE CORRENTE:""\n");
-      				scanf("%f", &y_min);
-      				printf("ENTRE COM O VALOR MAXIMO DA VARIAVEL MEDIDA:""\n");
-      				scanf("%f", &x_max);
-      				printf("ENTRE COM VALOR MINIMO DA VARIAVEL MEDIDA:""\n");
-      				scanf("%f", &x_min);
-      				printf("ENTRE COM O VALOR DO SENSOR:""\n");
-      				scanf("%f", &sensor_signal);
-      
-      				calc_cang();
-      				calc_sensor();  
-                 
-   	  				printf("\n""CONVERSAO: %.2f",sensor_final);
-   	  				getch();
-   	  			break;
-   	  			
-   	  			default:
-   	  				printf("\n""SINAL INVALIDO""\n");
-   	  				getch();
-   	  			break;
-   	  		}
-   	  }  	  				
+	switch(signal)
+	{
+		case 'V':
+			printf("\n""transdutor - TENSAO""\n");
+			printf("ENTRE COM O VALOR MAXIMO DE TENSAO:""\n");
+			scanf("%f", &y_max);
+			printf("ENTRE COM VALOR MINIMO DE TENSAO:""\n");
+			scanf("%f", &y_min);
+			printf("ENTRE COM O VALOR MAXIMO DA VARIAVEL MEDIDA:""\n");
+			scanf("%f", &x_max);
+			printf("ENTRE COM VALOR MINIMO DA VARIAVEL MEDIDA:""\n");
+			scanf("%f", &x_min);
+			printf("ENTRE COM O VALOR DO SENSOR:""\n");
+			scanf("%f", &sensor_signal);
+
+			calc_cang();
+			calc_sensor();  
+			
+			printf("\n""CONVERSAO: %.2f (U.M.)",sensor_final);
+		break;
+		
+		case 'A':
+			printf("\n""transdutor - CORRENTE""\n");
+			printf("ENTRE COM O VALOR MAXIMO DE CORRENTE:""\n");
+			scanf("%f", &y_max);
+			printf("ENTRE COM VALOR MINIMO DE CORRENTE:""\n");
+			scanf("%f", &y_min);
+			printf("ENTRE COM O VALOR MAXIMO DA VARIAVEL MEDIDA:""\n");
+			scanf("%f", &x_max);
+			printf("ENTRE COM VALOR MINIMO DA VARIAVEL MEDIDA:""\n");
+			scanf("%f", &x_min);
+			printf("ENTRE COM O VALOR DO SENSOR:""\n");
+			scanf("%f", &sensor_signal);
+
+			calc_cang();
+			calc_sensor();  
+			
+			printf("\n""CONVERSAO: %.2f (U.M.)",sensor_final);
+		break;
+		
+		default:
+			printf("\n""SINAL INVALIDO""\n");
+		break;
+	}
+   	  	  				
 			
 }
 
